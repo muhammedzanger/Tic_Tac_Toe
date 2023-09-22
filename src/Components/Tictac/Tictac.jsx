179e03b2e0631244eacc30f850bc9f7f771debf9
muhammedzanger <muhammedzanger13@gmail.com>
2023-09-23 02:41:20 +0300

    Tic_Tac_Toe-With React.js

---
+++ src/Components/Tictac/Tictac.jsx
@@ -0,0 +1,154 @@
+/** @format */
+
+import React, { useRef, useState } from "react"
+import "./tictac.css"
+import circle from "../Assets/circle.png"
+import cross from "../Assets/cross.png"
+
+let data = ["", "", "", "", "", "", "", "", ""]
+
+function Tictac() {
+  let [count, setCount] = useState(0)
+  let [lock, setLock] = useState(false)
+  let titleRef = useRef(null)
+  let box1 = useRef(null)
+  let box2 = useRef(null)
+  let box3 = useRef(null)
+  let box4 = useRef(null)
+  let box5 = useRef(null)
+  let box6 = useRef(null)
+  let box7 = useRef(null)
+  let box8 = useRef(null)
+  let box9 = useRef(null)
+  let boxArray = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
+  const toogle = (e, num) => {
+    if (lock) {
+      return 0
+    }
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src='${cross}'/>`
+      data[num] = "x"
+      setCount(++count)
+    } else {
+      e.target.innerHTML = `<img src='${circle}'/>`
+      data[num] = "o"
+      setCount(++count)
+    }
+    checkWin()
+  }
+  let checkWin = () => {
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      win(data[2])
+    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
+      win(data[5])
+    } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
+      win(data[8])
+    } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
+      win(data[6])
+    } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
+      win(data[7])
+    } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
+      win(data[8])
+    } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
+      win(data[8])
+    } else if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      win(data[2])
+    } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
+      win(data[6])
+    }
+  }
+  let win = (winner) => {
+    setLock(true)
+    if (winner === "x") {
+      titleRef.current.innerHTML = `Congratulation:- <img src='${cross}'/> Win`
+    } else {
+      titleRef.current.innerHTML = `Congratulation:- <img src='${circle}'/> Win`
+    }
+  }
+  let reset = () => {
+    data = ["", "", "", "", "", "", "", "", ""]
+    titleRef.current.innerHTML = `Tic Tac Toe Game In <span>React</span>`
+    boxArray.forEach((e) => {
+      e.current.innerHTML = ""
+      window.location.reload()
+    })
+  }
+  return (
+    <div className='container'>
+      <h1 className='title' ref={titleRef}>
+        Tic Tac Toe Game In <span>React</span>
+      </h1>
+      <div className='board'>
+        <div className='row1'>
+          <div
+            className='boxes'
+            ref={box1}
+            onClick={(e) => {
+              toogle(e, 0)
+            }}></div>
+          <div
+            className='boxes'
+            ref={box2}
+            onClick={(e) => {
+              toogle(e, 1)
+            }}></div>
+          <div
+            className='boxes'
+            ref={box3}
+            onClick={(e) => {
+              toogle(e, 2)
+            }}></div>
+        </div>
+        <div className='row2'>
+          <div
+            className='boxes'
+            ref={box4}
+            onClick={(e) => {
+              toogle(e, 3)
+            }}></div>
+          <div
+            className='boxes'
+            ref={box5}
+            onClick={(e) => {
+              toogle(e, 4)
+            }}></div>
+          <div
+            className='boxes'
+            ref={box6}
+            onClick={(e) => {
+              toogle(e, 5)
+            }}></div>
+        </div>
+        <div className='row3'>
+          <div
+            className='boxes'
+            ref={box7}
+            onClick={(e) => {
+              toogle(e, 6)
+            }}></div>
+          <div
+            className='boxes'
+            ref={box8}
+            onClick={(e) => {
+              toogle(e, 7)
+            }}></div>
+          <div
+            className='boxes'
+            ref={box9}
+            onClick={(e) => {
+              toogle(e, 8)
+            }}></div>
+        </div>
+      </div>
+      <button
+        className='reset'
+        onClick={() => {
+          reset()
+        }}>
+        Reset
+      </button>
+    </div>
+  )
+}
+
+export default Tictac
